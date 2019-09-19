344fbe51558384adbf904128f23415ae741f1f53
MoonAntonio <pinchodev91@gmail.com>
2019-09-19 14:25:11 +0200

    Base de la ventana

---
+++ src/Assets/XLight/Logica/Extensiones/WindowsExtension.cs
@@ -0,0 +1,26 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//                                                                              \\
+// WindowsExtension.cs (00/00/0000)                                             	\\
+// Autor: Antonio Mateo (.\Moon Antonio)    antoniomt.moon@gmail.com            \\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace Moon
+{
+	/// <summary>
+    /// <para></para>
+    /// </summary>
+	public class WindowsExtension : MonoBehaviour
+	{
+		#region Dev
+        [ContextMenu("Dev")]
+        protected void DevScript()
+        {
+            Debug.Log("<color=green>WindowsExtension :: DevScript()</color> >> Inicializado.");
+        }
+        #endregion
+	}
+}
